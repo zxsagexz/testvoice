5af97a689c56fab4273d42615e904ab5cdb450ca
zxsagexz <bot.error@Yandex.ru>
2017-10-14 17:06:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,19 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+
 client.on('ready', () => {
-    console.log('I am ready!');
+  console.log('I am ready!');
 });
 
+
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-})
+  if (message.content === 'what is my avatar') {
+    message.reply(message.author.avatarURL);
+  }
+});
 
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
+
