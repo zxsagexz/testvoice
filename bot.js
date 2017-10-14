f283d5a2213999f3302b4bce2d315fa4d9bbc032
zxsagexz <bot.error@Yandex.ru>
2017-10-14 18:02:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,25 +13,6 @@ client.on('message', message => {
   }
 });
 
-client.on('message', message => {
-  // Voice only works in guilds, if the message does not come from a guild,
-  // we ignore it
-  if (!message.guild) return;
-
-  if (message.content === '/join') {
-    // Only try to join the sender's voice channel if they are in one themselves
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          message.reply('I have successfully connected to the channel!');
-        })
-        .catch(console.log);
-    } else {
-      message.reply('You need to join a voice channel first!');
-    }
-  }
-});
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
 
