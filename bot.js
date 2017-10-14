17b1f567dc27fd54c7e2d54fa1c426bc634c35e6
zxsagexz <bot.error@Yandex.ru>
2017-10-14 17:13:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,10 +13,23 @@ client.on('message', message => {
   }
 });
 
-client.on('guildMemberAdd', member => {
-  const channel = member.guild.channels.find('name', 'member-log');
-  if (!channel) return;
-  channel.send(`Welcome to the server, ${member}`);
+client.on('message', message => {
+  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
+  if (!message.guild) return;
+
+  if (message.content === '/join') {
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+        .then(connection => { // Connection is an instance of VoiceConnection
+          message.reply('I have successfully connected to the channel!');
+        })
+        .catch(console.log);
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
+  }
 });
 
 // THIS  MUST  BE  THIS  WAY
