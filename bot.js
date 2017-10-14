6d22e1484cebed72407717cc4b87b826e5ecd8a5
zxsagexz <bot.error@Yandex.ru>
2017-10-14 16:55:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,80 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+client.on('message', m => {
+  if (!m.guild) return;
+  if (m.content.startsWith('/join')) {
+    const channel = m.guild.channels.get(m.content.split(' ')[1]) || m.member.voiceChannel;
+    if (channel && channel.type === 'voice') {
+      channel.join().then(conn => {
+        conn.player.on('error', (...e) => console.log('player', ...e));
+        if (!connections.has(m.guild.id)) connections.set(m.guild.id, { conn, queue: [] });
+        m.reply('ok!');
+      });
+    } else {
+      m.reply('Specify a voice channel!');
+    }
+  } else if (m.content.startsWith('/play')) {
+    if (connections.has(m.guild.id)) {
+      const connData = connections.get(m.guild.id);
+      const queue = connData.queue;
+      const url = m.content.split(' ').slice(1).join(' ')
+        .replace(/</g, '')
+        .replace(/>/g, '');
+      queue.push({ url, m });
+      if (queue.length > 1) {
+        m.reply(`OK, that's going to play after ${queue.length - 1} songs`);
+        return;
+      }
+      doQueue(connData);
+    }
+  } else if (m.content.startsWith('/skip')) {
+    if (connections.has(m.guild.id)) {
+      const connData = connections.get(m.guild.id);
+      if (connData.dispatcher) {
+        connData.dispatcher.end();
+      }
+    }
+  } else if (m.content.startsWith('#eval') && m.author.id === '127497944541822976') {
+    try {
+      const com = eval(m.content.split(' ').slice(1).join(' '));
+      m.channel.send(com, { code: true });
+    } catch (e) {
+      console.log(e);
+      m.channel.send(e, { code: true });
+    }
+  }
+});
+
+function doQueue(connData) {
+  const conn = connData.conn;
+  const queue = connData.queue;
+  const item = queue[0];
+  if (!item) return;
+  const stream = ytdl(item.url, { filter: 'audioonly' }, { passes: 3 });
+  const dispatcher = conn.playStream(stream);
+  stream.on('info', info => {
+    item.m.reply(`OK, playing **${info.title}**`);
+  });
+  dispatcher.on('end', () => {
+    queue.shift();
+    doQueue(connData);
+  });
+  dispatcher.on('error', (...e) => console.log('dispatcher', ...e));
+  connData.dispatcher = dispatcher;
+}
+
+
+
+
+/*
 client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
-});
+});*/
+
+
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.MzY4NzU2OTAyMjMxODAxODU2.DMOnJw.QY7_AWlPD6pmVugtvqJ0CWGzMbM);
+client.login(process.env.BOT_TOKEN);
