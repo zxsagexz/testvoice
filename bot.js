40c658fd462c234d920bbfece2b654ae019574fb
zxsagexz <bot.error@Yandex.ru>
2017-10-14 18:08:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,15 +14,12 @@ client.on('message', message => {
 });
 
 client.on('message', function(message) {
-    if (message.content == "!clear") {
-        try {
-            if (message.member.hasPermission("MANAGE_MESSAGES")) {
-                messages = message.channel.fetchMessages();
-                message.channel.bulkDelete(messages);
-            }
-        } catch(e) {
-            message.channel.send("ERROR: ERROR CLEARING CHANNEL.");
-            console.log(e);
+    if (message.content == "^clear") {
+        if (message.member.hasPermission("MANAGE_MESSAGES")) {
+            message.channel.fetchMessages()
+               .then(function(list){
+                    message.channel.bulkDelete(list);
+                }, function(err){message.channel.send("ERROR: ERROR CLEARING CHANNEL.")})                        
         }
     }
 
